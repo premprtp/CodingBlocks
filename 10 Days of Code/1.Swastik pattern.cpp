c8ba4dcc42d4a7187055c7e8ca0c9f18e47d026a
Prem Pratap <theprempratap@gmail.com>
2020-06-01 21:30:25 +0530

    pattern

---
+++ 10 Days of Code/1.Swastik pattern.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	//first component
+	//star
+	cout<<"*";
+
+	//space
+	for (int i=1;i<=(n-3)/2;i++){
+		cout<<" ";
+	}
+	//star
+	for (int i=1;i<=(n+1)/2;i++){
+		cout<<"*";
+	}
+	cout<<endl;
+
+	//second component
+	//star
+	for (int row=1;row<=(n-3)/2;row++){
+		cout<<"*";
+		//space
+		for (int i=1;i<=(n-3)/2;i++){
+			cout<<" ";
+		}
+		cout<<"*"<<endl;
+	}
+
+
+	//third component
+	//star
+	for (int i=1;i<=n;i++){
+		cout<<"*";
+	}
+	cout<<endl;
+
+	//fourth component
+	//space
+	for (int row=1;row<=(n-3)/2;row++){
+		//space
+		for(int i=1;i<=(n-3)/2+1;i++){
+			cout<<" ";
+		}
+
+		//star
+		cout<<"*";
+		//space
+		for(int i=1;i<=(n-3)/2;i++){
+			cout<<" ";
+		}
+
+		//star
+		cout<<"*"<<endl;
+	}
+
+	//fifth compontent
+	//star
+	for (int i=1;i<=(n+1)/2;i++){
+		cout<<"*";
+	}
+	//space
+	for (int i=1;i<=(n-3)/2;i++){
+		cout<<" ";
+	}
+	//star
+	cout<<"*";
+	return 0;
+
+}
