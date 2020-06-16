d3ec0f35f6d347a6883aea6388776731b721cf5a
Prem Pratap <theprempratap@gmail.com>
2020-06-16 13:26:50 +0530

    completed

---
+++ 10 Days of Code/9.Divisible subarray.cpp
@@ -0,0 +1,41 @@
+#Divisible Subarrays |[Problem](https://hack.codingblocks.com/app/practice/6/1065/problem)
+
+
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+#define ll long
+
+ll a[1000005], pre[1000005];
+
+int main() {
+
+	int t;
+	cin >>t;
+	while (t--) {
+		int n;
+		cin>>n;
+		memset(pre,0,sizeof(pre));
+
+		pre[0]=1;
+		//read the input
+		int sum=0;
+		for (int i=0;i<n;i++){
+			cin>>a[i];
+			sum +=a[i];
+			sum %=n;
+			sum = (sum+n)%n;
+			pre[sum]++;
+		}
+		ll ans=0;
+		for(int i=0;i<n;i++){
+			ll m=pre[i];
+			ans +=(m)*(m-1)/2;
+		}
+		cout<<ans<<endl;
+	}
+	
+	return 0;
+}
